6a5e70e0c232179169b18754ed39bc79c21413ee
Thomas M. Hermann <thermann@loadsgroup.com>
2016-02-16 13:48:47 -0600

    Prototype data parsing object to facilitate scanning files.

---
+++ dataParser.m
@@ -0,0 +1,45 @@
+classdef dataParser < handle
+    %dataParser Parsing object for files with irregular data
+    
+    properties
+        line, dataCell, lastline
+    end
+    
+    methods
+        function instance = dataParser( varargin )
+            % Input validation
+            ip = inputParser;
+            ip.addOptional('theData', cell(0), @iscellstr)
+            ip.parse(varargin{:})
+            % Initialize the instance
+            instance.line = 0;
+            instance.dataCell = ip.Results.theData;
+            instance.lastline = numel(ip.Results.theData);
+        end
+        function theLine = next( self, count )
+            % Default to 1 line
+            if nargin == 1
+                count = 1;
+            end
+            % Increment and return the next line
+            lineNumber = self.line + count;
+            if lineNumber < self.lastline
+                self.line = lineNumber;
+                theLine = self.dataCell{lineNumber};
+            else
+                error('Next line, %i, exceeds lastline, %i.', ...
+                    lineNumber, self.lastline)
+            end
+        end
+        function rewind( self )
+            self.line = 0;
+        end
+        function torf = eod( self, count )
+            if nargin == 1
+                count = 1;
+            end
+            torf = self.lastline <= self.line + count;
+        end
+    end
+    
+end
